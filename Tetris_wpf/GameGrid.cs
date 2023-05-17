d2e5632f11bb7d7f695cc8bcb67d14c5b4a11d41
Bakkonrad <konri0908@gmail.com>
2023-05-17 12:17:56 +0200

    Add project files.

---
+++ Tetris_wpf/GameGrid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_wpf
+{
+    internal class GameGrid
+    {
+        private readonly int[,] grid; //dwuwymiarowa tablica
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c] //indekser
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid(int rows, int columns) //konstruktor
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int r, int c) //czy dane miejsce jest wewnątrz planszy
+        {
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+        }
+
+        public bool IsEmpty(int r, int c) //czy dane miejsce jest puste
+        {
+            return IsInside(r, c) && grid[r, c] == 0;
+        }
+
+        public bool IsRowFull(int r) //czy rząd jest pełny
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsRowEmpty(int r) //czy rząd jest pusty
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
